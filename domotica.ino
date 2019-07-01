7387f75cf72d2e5ca16a300bba1633de8995526b
Ariel <alexanderaguayo43@gmail.com>
2019-07-01 00:27:13 -0500

    Control de temperatura (Falta humedad)
    Lee y controlo temperatura, pero solo lee humedad

--- domotica.ino
+++ domotica.ino
@@ -1,6 +1,11 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Servo.h>
+#include <dht_nonblocking.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+
+static const int DHT_SENSOR_PIN = 6;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
  
 #define SS_PIN 53
 #define RST_PIN 10
@@ -20,11 +25,47 @@ void setup()
   pinMode(LED_R, OUTPUT);
   Serial.println("Pase la tarjeta");
   Serial.println();
+  pinMode(22,OUTPUT);
 
 }
+static bool measure_environment( float *temperature, float *humidity ){
+  static unsigned long measurement_timestamp = millis( );
+
+  /* Measure once every four seconds. */
+  if( millis( ) - measurement_timestamp > 3000ul )
+  {
+    if( dht_sensor.measure( temperature, humidity ) == true )
+    {
+      measurement_timestamp = millis( );
+      return( true );
+    }
+  }
+
+  return( false );
+}
+
 void loop() 
 {
- 
+ float temperature;
+  float humidity;
+
+  /* Measure temperature and humidity.  If the functions returns
+     true, then a measurement is available. */
+  if( measure_environment( &temperature, &humidity ) == true )
+  {
+    Serial.print( "T = " );
+    Serial.print( temperature, 1 );
+    Serial.print( " deg. C, H = " );
+    Serial.print( humidity, 1 );
+    Serial.println( "%" );
+
+    if (temperature > 26){
+      digitalWrite(22,HIGH);
+    }
+    else if( temperature<=26){
+      digitalWrite(22,LOW);
+    }
+  }
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
     return;
