70aa45a03b5aeed716a876da7726856d0771cc63
Alexander <47733425+iamqwertyuiopz@users.noreply.github.com>
2019-12-05 21:28:52 -0500

    Añadido luces de otras habitaciones
    Se añadieron luces de otras habitaciones, eliminar funciones que no se usan, pruebas de PIR y correcciòn de errores :)

--- domoticaUNO.ino
+++ domoticaUNO.ino
@@ -1,14 +1,14 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial BT(2, 3); // RX, TX
-
+int luzBalcon = 7;
+int luzGaraje = 8;
 int luzSala = 13;
 int luzCuarto = 9; 
 int luzCocina = 11;
 int luzFuera = 10;
 const int ldrPin = A0;
 byte sensorpir = 6;
-char phone_no[]="+593983842830";
 int actSensor = 5;
 char valor ;  //Variable para indicar que llega una orden
 
@@ -25,22 +25,21 @@ void setup() {
   pinMode(ldrPin, INPUT);
   Serial.begin(9600);
   digitalWrite(actSensor, HIGH);
+  pinMode(luzGaraje, OUTPUT);
+  pinMode(luzBalcon, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int ldrStatus = analogRead(ldrPin);
-
   if (BT.available())  //Si el puerto serie (Bluetooth) está disponible
   {
     valor = BT.read();  //Lee el dato entrante via Bluetooth
   }
-    if (valor == 'A' || ldrStatus <= 200)   
+    if (valor == 'A')   
     {
       digitalWrite(luzFuera, HIGH);  
     }
-    if (valor == 'B' || ldrStatus > 200)  
+    if (valor == 'B')  
     {
       digitalWrite(luzFuera, LOW);  
     }
@@ -68,12 +67,29 @@ void loop() {
     {
       digitalWrite(luzCuarto, LOW);   
     }
+    if (valor == 'K')   
+    {
+      digitalWrite(luzGaraje, HIGH);  
+    }
+    if (valor == 'L') 
+    {
+      digitalWrite(luzGaraje, LOW); 
+    }
+    if (valor == 'I')   
+    {
+      digitalWrite(luzBalcon, HIGH);  
+    }
+    if (valor == 'J') 
+    {
+      digitalWrite(luzBalcon, LOW); 
+    }
+
+
     if (valor == 'X' ){
       if(digitalRead(sensorpir) == HIGH){
         alarma();
       
       delay(1000);
-      llamada();
       digitalWrite(actSensor,LOW);
       }
       }
@@ -81,7 +97,7 @@ void loop() {
       digitalWrite(7,LOW);
       digitalWrite(actSensor,HIGH);
     }
-
+    
 }
 
 void alarma(){
@@ -93,15 +109,4 @@ void alarma(){
       delay(500);
       digitalWrite(7,LOW);
       delay(500);
-}
-
-void llamada(){
-delay(400);
-Serial.println("AT");
-delay(1000);
-Serial.print("ATD");
-Serial.println(phone_no);
-//Serial.println(";");
-delay(30000);
-Serial.println("ATH");
 }
